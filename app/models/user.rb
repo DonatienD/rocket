109121992c4a40a62a3d677389357dc017a5f6b8
Donatien D <ddlu.web@icloud.com>
2020-12-01 18:21:49 +0100

    profiles controller (#new & #create) and subjects controller (#new & #create) created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :profiles
+  has_one :profile
   has_many :subjects
 end
