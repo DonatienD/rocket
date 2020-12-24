d8e6c5e18b4d49a79b428b05c1f3e81ccea02659
Donatien D <ddlu.web@icloud.com>
2020-12-24 14:52:11 +0100

    Added ability as a student to remove access to a teacher room

---
+++ test/integration/delete_access_test.rb
@@ -0,0 +1,78 @@
+require 'test_helper'
+
+class DeleteAccessTest < ActionDispatch::IntegrationTest
+
+  test "Student should be able to delete an access he created to teacher room" do
+    sign_in users(:student)
+
+    @student_access = accesses(:student_access_to_teacher_room)
+
+    assert_difference 'Access.count', -1, "Access was not deleted" do
+      delete "/accesses/#{@student_access.id}"
+    end
+
+    assert_not Access.all.include?(@student_access), "Access was not deleted"
+  end
+
+  test "Student shouldn't be able to delete an access to it's own room" do
+    sign_in users(:student)
+
+    @student_access = accesses(:student_access)
+
+    assert_no_difference 'Access.count', "Access not deleted" do
+      delete "/accesses/#{@student_access.id}"
+    end
+
+    assert Access.all.include?(@student_access), "Access not deleted"
+  end
+
+  test "Teacher should not be able to delete an access to its own room" do
+    sign_in users(:teacher)
+
+    @teacher_access = accesses(:teacher_access)
+
+    assert_no_difference 'Access.count', "Access was deleted" do
+      delete "/accesses/#{@teacher_access.id}"
+    end
+
+    assert Access.all.include?(@teacher_access), "Access was deleted"
+  end
+
+  test "Teacher should not be able to delete an student access" do
+    sign_in users(:teacher)
+
+    @student_access = accesses(:student_access_to_teacher_room)
+
+    assert_no_difference 'Access.count', "Access was deleted" do
+      delete "/accesses/#{@student_access.id}"
+    end
+
+    assert Access.all.include?(@student_access), "Access was deleted"
+  end
+
+  # test "User should not be able to delete a room he did not create" do
+  #   # Student shouldn't delete teacher's room
+  #   sign_in users(:student)
+
+  #   @teacher_room = rooms(:teacher_room)
+
+  #   assert_no_difference 'Room.count', "Teacher room was deleted by student" do
+  #     delete "/rooms/#{rooms(:teacher_room).id}"
+  #   end
+
+  #   assert Room.all.include?(@teacher_room), "Teacher room was deleted by student"
+
+  #   sign_out :student
+
+  #   # Teacher shouldn't delete student's room
+  #   sign_in users(:teacher)
+
+  #   @student_room = rooms(:student_room)
+
+  #   assert_no_difference 'Room.count', "Student room was deleted by Teacher" do
+  #     delete "/rooms/#{rooms(:student_room).id}"
+  #   end
+
+  #   assert Room.all.include?(@student_room), "Student room was deleted by Teacher"
+  # end
+end
